e6afb21188d288cb2d669636d5256a68a90d65a6
Daiane Azevedo <daianeaze16@gmail.com>
2021-01-26 17:16:03 -0300

    Tela inicial de opção de jogos

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,27 +1,28 @@
 import random
-print("***************************************")
-print("Bem vindo ao jogo de Adivinhação!")
-print("***************************************")
+def jogar():
+    print("***************************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("***************************************")
 
-numero_secreto = random.randrange(1, 101)
-maximo_tentativas = 3
-rodada = 1
+    numero_secreto = random.randrange(1, 101)
+    maximo_tentativas = 3
+    rodada = 1
 
-for rodada in range(1, maximo_tentativas + 1 ):
-    print("Tentativa {} de {}".format(rodada, maximo_tentativas))
-    chute = int(input("Digite o seu numero entre 1 e 100:"))
-    print("Você digitou o número", chute)
-    acertou = numero_secreto == chute
-    maior   = numero_secreto < chute
-    menor   = numero_secreto > chute
+    for rodada in range(1, maximo_tentativas + 1 ):
+        print("Tentativa {} de {}".format(rodada, maximo_tentativas))
+        chute = int(input("Digite o seu numero entre 1 e 100:"))
+        print("Você digitou o número", chute)
+        acertou = numero_secreto == chute
+        maior   = numero_secreto < chute
+        menor   = numero_secreto > chute
 
-    if (acertou):
-        print("Você acertou!")
-        break
-    else:
-        print("Você errou.")
-        if(maior):
-            print("Seu chute foi maior que o número.")
-        elif(menor):
-            print("Seu chute foi menor que o número.")
-print("Fim de jogo.")
+        if (acertou):
+            print("Você acertou!")
+            break
+        else:
+            print("Você errou.")
+            if(maior):
+                print("Seu chute foi maior que o número.")
+            elif(menor):
+                print("Seu chute foi menor que o número.")
+    print("Fim de jogo.")
