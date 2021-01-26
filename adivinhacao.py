e3bd3e676054595deca3f5e7e83d72f943be8c40
Daiane Azevedo <daianeaze16@gmail.com>
2021-01-26 12:27:33 -0300

    Exercicios com loop e dicas

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,10 +1,27 @@
 print("***************************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("***************************************")
+
 numero_secreto = 20
-chute = int(input("Digite o seu numero:"))
-print("Você digitou o número", chute)
-if (numero_secreto == chute):
-    print("Você acertou!")
-else:
-    print("Você errou.")
+maximo_tentativas = 3
+rodada = 1
+
+while(rodada <= maximo_tentativas):
+    print("Tentativa {} de {}".format(rodada, maximo_tentativas))
+    chute = int(input("Digite o seu numero:"))
+    print("Você digitou o número", chute)
+    acertou = numero_secreto == chute
+    maior   = numero_secreto < chute
+    menor   = numero_secreto > chute
+
+    if (acertou):
+        print("Você acertou!")
+        break
+    else:
+        print("Você errou.")
+        if(maior):
+            print("Seu chute foi maior que o número.")
+        elif(menor):
+            print("Seu chute foi menor que o número.")
+    rodada = rodada + 1
+print("Fim de jogo.")
