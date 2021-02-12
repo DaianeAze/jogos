bd3d496f61dd0c41280c0cfd8fb78c9547ed44fb
Daiane Azevedo <daianeaze16@gmail.com>
2021-02-12 11:35:11 -0300

    Jogo de forca está funcional

--- forca.py
+++ forca.py
@@ -2,3 +2,38 @@ def jogar():
     print("***************************************")
     print("Bem vindo ao jogo de Forca!")
     print("***************************************")
+
+    palavra_secreta = 'abacaxi'.upper()
+    letras_acertadas = ["_", "_", "_", "_", "_", "_", "_"]
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    while (not enforcou and not acertou):
+        chute = input("Qual a letra?")
+        chute = chute.strip().upper()
+
+        index = 0
+        if (chute in palavra_secreta):
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Trecho não existe na palavra.")
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if (enforcou):
+        print("SE ENFORCOU! Você excedeu o limite de tentativas.")
+    if (acertou):
+        print("PARABÉNS! Você acertou a palavra antes de se enforcar.")
+    print("Fim de jogo")
+
+
+if (__name__ == "__main__"):
+    jogar()
